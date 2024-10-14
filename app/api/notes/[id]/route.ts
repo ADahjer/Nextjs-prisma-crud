f143468bf1bdc67be29164e2b2b15ce5a2c4a56e
Asmith Dahjer <asmith200205@gmail.com>
2024-10-14 18:39:01 -0500

    add rest of CRUD logic

--- app/api/notes/[id]/route.ts
+++ app/api/notes/[id]/route.ts
@@ -1,11 +1,72 @@
+import { prisma } from "@/utils/prisma";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { NextResponse } from "next/server";
 
-export function GET() {}
-
-export function DELETE() {
-  return NextResponse.json({ message: "Deleting note" });
+interface Params {
+  params: { id: string };
 }
 
-export function PUT() {
-  return NextResponse.json({ message: "Updating note" });
+function ApiError(error: Error) {
+  if (error instanceof PrismaClientKnownRequestError) {
+    if (error.code === "P2025") {
+      return NextResponse.json({
+        error: "Note not found",
+        status: 404,
+      });
+    }
+  }
+
+  return NextResponse.json({ error: error.message, status: 500 });
+}
+
+export async function GET(request: Request, { params }: Params) {
+  try {
+    const note = await prisma.note.findFirst({
+      where: {
+        id: parseInt(params.id),
+      },
+    });
+
+    if (!note) {
+      return NextResponse.json({ error: "Note not found", status: 404 });
+    }
+
+    return NextResponse.json(note);
+  } catch (error) {
+    if (error instanceof Error) return ApiError(error);
+  }
+}
+
+export async function DELETE(request: Request, { params }: Params) {
+  try {
+    const deletedNote = await prisma.note.delete({
+      where: {
+        id: parseInt(params.id),
+      },
+    });
+
+    return NextResponse.json(deletedNote);
+  } catch (error) {
+    if (error instanceof Error) return ApiError(error);
+  }
+}
+
+export async function PUT(request: Request, { params }: Params) {
+  try {
+    const { title, content } = await request.json();
+
+    const updatedNote = await prisma.note.update({
+      where: {
+        id: parseInt(params.id),
+      },
+      data: {
+        title,
+        content,
+      },
+    });
+
+    return NextResponse.json(updatedNote);
+  } catch (error) {
+    if (error instanceof Error) return ApiError(error);
+  }
 }
